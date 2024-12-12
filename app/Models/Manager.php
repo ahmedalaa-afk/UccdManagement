d106984b11d3189f1ed47938fffae997e46e648b
Ahmed Alaa <ahmed.alaaeldine21@gmail.com>
2024-12-12 18:00:01 +0200

    all manager have one account to login

--- app/Models/Manager.php
+++ app/Models/Manager.php
@@ -10,5 +10,34 @@ use Spatie\Permission\Traits\HasRoles;
 class Manager extends Model
 {
     /** @use HasFactory<\Database\Factories\ManagerFactory> */
-    use HasFactory,HasApiTokens,HasRoles;
+    use HasFactory, HasApiTokens, HasRoles;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
--- database/seeders/ManagerSeeder.php
+++ database/seeders/ManagerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Manager;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,10 @@ class ManagerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Manager::create([
+        //     'name' => 'Manager',
+        //     'email' => 'uccdmanager@gmail.com',
+        //     'password' => bcrypt('123456789'),
+        // ]);
     }
 }
